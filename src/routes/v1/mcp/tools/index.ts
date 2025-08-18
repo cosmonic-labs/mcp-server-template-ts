9c7caa902b6d481e9de37bef0d18aab6bd064d3c
Bailey Hayes <bailey@cosmonic.com>
2025-08-18 10:29:05 -0400

    fix: change sample tool to simple echo

--- src/routes/v1/mcp/tools/index.ts
+++ src/routes/v1/mcp/tools/index.ts
@@ -1,6 +1,6 @@
 import { McpServer as UpstreamMCPServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 
-import * as startNotificationStream from "./start-notification-stream.js";
+import * as startNotificationStream from "./echo.js";
 
 export function setupAllTools<S extends UpstreamMCPServer>(server: S) {
   startNotificationStream.setupTool(server);
