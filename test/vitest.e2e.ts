69dbf1f4bdd187406734c25e17647432ab5d10f9
Victor Adossi <vadossi@cosmonic.com>
2025-08-22 00:50:25 +0900

    refactor(tests): allow retries only in CI

--- test/vitest.e2e.ts
+++ test/vitest.e2e.ts
@@ -1,3 +1,4 @@
+import { env } from "node:process";
 import { defineConfig } from "vitest/config";
 
 const DEFAULT_TIMEOUT_MS = 1000 * 60 * 10; // 10m
@@ -9,7 +10,8 @@ const REPORTERS = process.env.GITHUB_ACTIONS
 export default defineConfig({
   test: {
     reporters: REPORTERS,
-    retry: 3,
+    // Github Actions machines, in particular windows can be flaky
+    retry: env.CI ? 3 : 0,
     disableConsoleIntercept: true,
     printConsoleTrace: true,
     passWithNoTests: false,
