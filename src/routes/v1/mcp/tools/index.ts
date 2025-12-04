9732dc7a01657687819fb7ecd6c9cfe792b57af3
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2025-12-04 13:58:10 -0500

    Simplify MCPServer argument

--- src/routes/v1/mcp/tools/index.ts
+++ src/routes/v1/mcp/tools/index.ts
@@ -1,7 +1,7 @@
-import { McpServer as UpstreamMCPServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { MCPServer } from "../server.js";
 
 import * as startNotificationStream from "./echo.js";
 
-export function setupAllTools<S extends UpstreamMCPServer>(server: S) {
+export function setupAllTools(server: MCPServer) {
   startNotificationStream.setupTool(server);
 }
