3c76d2145aaa0f00af3babc4305b31220a338889
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2026-01-07 14:14:01 -0500

    Don't import with .js extension

--- src/routes/v1/mcp/index.ts
+++ src/routes/v1/mcp/index.ts
@@ -1,6 +1,6 @@
 import type { Hono } from "hono";
-import { MCPServer } from "./server.js";
-import { MCP_SERVER_BASE_PATH } from "../../../config.js";
+import { MCPServer } from "./server";
+import { MCP_SERVER_BASE_PATH } from "../../../config";
 
 export function setupRoutes(app: Hono) {
   app.post(MCP_SERVER_BASE_PATH, MCPServer.handleMCPRequest);
--- src/routes/v1/mcp/tools/index.ts
+++ src/routes/v1/mcp/tools/index.ts
@@ -1,6 +1,6 @@
-import { MCPServer } from "../server.js";
+import { MCPServer } from "../server";
 
-import * as startNotificationStream from "./echo.js";
+import * as startNotificationStream from "./echo";
 
 export function setupAllTools(server: MCPServer) {
   startNotificationStream.setupTool(server);
